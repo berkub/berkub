ecb230dae01329d9e5730555514c6c8fda86ca4b
Adam Miribyan <adam.miribyan@gmail.com>
2013-12-23 00:17:56 +0100

    Add PasswordsController. Add and style the passwords#new page.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :passwords
 
   # You can have the root of your site routed with "root"
   root "welcome#index"
