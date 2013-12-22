7cff6fd3fdd57c567ae07195cf9ad7c7f4753654
Adam Miribyan <adam.miribyan@gmail.com>
2013-12-22 01:18:50 +0100

    Signup page styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :first_name, :last_name, :email, :password, presence: true
-	validates :password_confirmation, presence: true, on: :create
 	validates :email, uniqueness: true
 
 	def full_name
